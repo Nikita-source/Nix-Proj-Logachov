663b58172e6a290f1c18bb147eefa896c92a8c55
Nikita <logachovnik@gmail.com>
2021-07-19 00:40:30 +0300

    add property setter

--- unit_11_reflection/src/main/java/ApplyProperties.java
+++ unit_11_reflection/src/main/java/ApplyProperties.java
@@ -10,9 +10,8 @@ public class ApplyProperties {
         try {
             Properties props = loadProperties();
             T instance = propertyClass.getDeclaredConstructor().newInstance();
-            Field[] fields = propertyClass.getDeclaredFields();
 
-            for (Field field : fields) {
+            for (Field field : propertyClass.getDeclaredFields()) {
                 if (!field.isAnnotationPresent(PropertyKey.class)) {
                     continue;
                 }
@@ -28,18 +27,18 @@ public class ApplyProperties {
 
                 if (type == String.class) {
                     field.set(instance, value);
-                } else if (type == char.class || type == Character.class) {
-                    field.set(instance, value.charAt(0));
                 } else if (type == int.class || type == Integer.class) {
                     field.set(instance, Integer.parseInt(value));
+                } else if (type == boolean.class || type == Boolean.class) {
+                    field.set(instance, Boolean.parseBoolean(value));
+                } else if (type == char.class || type == Character.class) {
+                    field.set(instance, value.charAt(0));
                 } else if (type == long.class || type == Long.class) {
                     field.set(instance, Long.parseLong(value));
                 } else if (type == float.class || type == Float.class) {
                     field.set(instance, Float.parseFloat(value));
                 } else if (type == double.class || type == Double.class) {
                     field.set(instance, Double.parseDouble(value));
-                } else if (type == boolean.class || type == Boolean.class) {
-                    field.set(instance, Boolean.parseBoolean(value));
                 } else if (type.isEnum()) {
                     Enum en = Enum.valueOf((Class<Enum>) type, value);
                     field.set(instance, en);
